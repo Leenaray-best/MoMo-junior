95a2b27d2c15a0250a5cc36c59e48309f5fccb7a
Leena <audrey.trova@gmail.com>
2022-08-16 11:51:53 +0200

    Update1.5

---
+++ commands/tools/gererxp.js
@@ -0,0 +1,105 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+var authId = JSON.parse(fs.readFileSync("./auth.json"));
+const mongoose = require("mongoose");
+const ficheBonus = require("../../salonBonus");
+const fichePerso = require("../../FichePerso");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("gererxp")
+    .setDescription("Info pour le joueur")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("remove")
+        .setDescription("Remove XP to the User")
+        .addUserOption((option) =>
+          option.setName("joueur").setDescription("The user").setRequired(true)
+        )
+        .addNumberOption((option) =>
+          option
+            .setName("xp")
+            .setDescription("Enter a number")
+            .setRequired(true)
+        )
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("give")
+        .setDescription("Give XP to the User")
+        .addUserOption((option) =>
+          option.setName("joueur").setDescription("The user").setRequired(true)
+        )
+        .addNumberOption((option) =>
+          option
+            .setName("xp")
+            .setDescription("Enter a number")
+            .setRequired(true)
+        )
+    ),
+  async execute(interaction, client) {
+    const message = await interaction.deferReply({
+      fetchReply: true,
+    });
+    // if (!interaction.isChatInputCommand()) return;
+    const user = interaction.user;
+    const channelMessage = interaction.channelId;
+    if (
+      (user.id == authId.staff.emi ||
+        user.id == authId.staff.leena ||
+        user.id == authId.staff.meri) &&
+      channelMessage == authId.Salon.SalonBotAdmin
+    ) {
+      if (interaction.commandName === "gererxp") {
+        let guildBonus = await ficheBonus.findOne({
+          _id: authId.idDatabase.BonusId,
+        });
+        // Add a channel to Earth bender
+        if (interaction.options.getSubcommand("remove")) {
+          const gamer = interaction.options.getUser("joueur");
+          const valueXP = interaction.options.getNumber("xp");
+          console.log(gamer.id);
+          const idGamer = gamer.id;
+          let guildPerso = await fichePerso.findOne({
+            _id: idGamer,
+          });
+          console.log(guildPerso);
+          const oldNiveauXP = guildPerso.NiveauXP;
+          const newNiveauXP = oldNiveauXP - valueXP;
+          await ficheBonus.findOneAndUpdate(
+            { _id: authId.idDatabase.BonusId },
+            { NiveauXP: newNiveauXP }
+          );
+          newMessage = `${valueXP} XP ont été retiré à ${guildPerso.Identite.Prenom} ${guildPerso.Identite.Nom}`;
+          await interaction.editReply({
+            content: newMessage,
+          });
+        } else if (interaction.options.getSubcommand("give")) {
+          const gamer = interaction.options.getUser("joueur");
+          const valueXP = interaction.options.getNumber("xp");
+          console.log(gamer.id);
+          const idGamer = gamer.id;
+          let guildPerso = await fichePerso.findOne({
+            _id: idGamer,
+          });
+          console.log(guildPerso);
+          const oldNiveauXP = guildPerso.NiveauXP;
+          const newNiveauXP = oldNiveauXP + valueXP;
+          await ficheBonus.findOneAndUpdate(
+            { _id: authId.idDatabase.BonusId },
+            { NiveauXP: newNiveauXP }
+          );
+          newMessage = `${valueXP} XP a été atrtibué à ${guildPerso.Identite.Prenom} ${guildPerso.Identite.Nom}`;
+          await interaction.editReply({
+            content: newMessage,
+          });
+        }
+      }
+    } else {
+      newMessage = `Tu n'as pas les autorisations pour faire ça`;
+      await interaction.editReply({
+        content: newMessage,
+      });
+    }
+  },
+};
