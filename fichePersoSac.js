af7a55f8ec58a31616575be476eb50454f668626
Leena <audrey.trova@gmail.com>
2022-08-21 18:29:15 +0200

    Update1.7

--- fichePersoSac.js
+++ fichePersoSac.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bagPerso = mongoose.Schema({
   _id: String,
-  Competence: [Number, Number, Number, Number, Number, Number, Number, Number],
+  Competence: [String],
   Sac: [String],
   time: Date,
 });
