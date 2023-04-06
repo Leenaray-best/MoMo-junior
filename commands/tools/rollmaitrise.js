5de78907006bf346b6bbddaa92f9e1f09cbb4c7f
Leena <audrey.trova@gmail.com>
2023-04-06 23:28:05 +0200

    FixedRoleRollTwice

--- commands/tools/rollmaitrise.js
+++ commands/tools/rollmaitrise.js
@@ -153,7 +153,7 @@ module.exports = {
               message.createdAt.getHours() <= 3
             ) {
               console.log(
-                "Ton perso est bien dans un salon RP et il fait nuit"
+                "Ton perso est bien dans un salon RP et il fait nuit donc je passe par la "
               );
               if (guildMeteo.Nuit == guildBonus.EauBonusNui[0]) {
                 var BonusSup = Number(2);
