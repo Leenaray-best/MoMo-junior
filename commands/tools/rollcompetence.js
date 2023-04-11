f6caa04abf3a0c8095e55d3de3efe932ef49203a
Leena <audrey.trova@gmail.com>
2023-04-11 18:06:13 +0200

    AddAchatRoll

--- commands/tools/rollcompetence.js
+++ commands/tools/rollcompetence.js
@@ -215,11 +215,11 @@ module.exports = {
           await interaction.deleteReply();
         }
       } else {
-        console.log("PAS DANS LE BON SALON");
-        const newMessage = `Tu n'as pas les autorisations pour faire ça, ou tu n'es pas dans la bon salon. Cette commande se fait seulement dans un salon de Rp`;
-        await interaction.editReply({
-          content: newMessage,
-        });
+        console.log("Pas dans le bon salon");
+        //const newMessage = `Tu n'as pas les autorisations pour faire ça, ou tu n'es pas dans la bon salon. Cette commande se fait seulement dans un salon de Rp`;
+        //await interaction.editReply({
+        //  content: newMessage,
+        //});
       }
     }
   },
