9488b3a8520c9606620efc5067054a357091bb5c
Leena <audrey.trova@gmail.com>
2023-04-12 17:05:59 +0200

    AddBuyMultiAchat-4

--- commands/tools/use.js
+++ commands/tools/use.js
@@ -13,9 +13,9 @@ module.exports = {
     .setDescription("Utiliser une potion ou un poison")
     .addUserOption((option) =>
       option
-        .setName("target")
+        .setName("joueur")
         .setRequired(true)
-        .setDescription("to show the targeted user's tag")
+        .setDescription("La personne surlequel l'objet s'applique")
     )
     .addStringOption((option) =>
       option
@@ -34,6 +34,8 @@ module.exports = {
     // if (!interaction.isChatInputCommand()) return;
     const user = interaction.user;
     IdPerso = user.id;
+    PersoAppliqueObjet = interaction.options.getUser("joueur");
+    IdPersoAppliqueObjet = PersoAppliqueObjet.id;
     const channelMessage = interaction.channelId;
     console.log(channelMessage);
     console.log(authId.Salon.SalonBotAdmin);
@@ -41,10 +43,10 @@ module.exports = {
       if (interaction.commandName === "use") {
         if (interaction.options.getString("categorie") === "potion") {
           let fiche = await fichePerso.findOne({
-            _id: IdPerso,
+            _id: IdPersoAppliqueObjet,
           });
           let ficheSac = await ficheBagPerso.findOne({
-            _id: IdPerso,
+            _id: IdPersoAppliqueObjet,
           });
           const nombrePotionOld = ficheSac.NbrePotion;
           if (interaction.member.roles.cache.has(authId.RoleRP.TheLiang)) {
@@ -57,21 +59,101 @@ module.exports = {
               interaction.member.roles.add(authId.RoleRP.TheLiang);
               var nombrePotionNew = nombrePotionOld - 1;
               await ficheBagPerso.updateMany(
-                { _id: user.id },
+                { _id: IdPersoAppliqueObjet },
                 { $pull: { Sac: { $in: [`${nombrePotionOld} Potion(s)`] } } }
               );
               await ficheBagPerso.findOneAndUpdate(
-                { _id: IdPerso },
+                { _id: IdPersoAppliqueObjet },
                 { $push: { Sac: `${nombrePotionNew} Potion(s)` } }
               );
               await ficheBagPerso.findOneAndUpdate(
-                { _id: IdPerso },
-                { Tour: 5, NbrePotion: nombrePotionNew }
+                { _id: IdPersoAppliqueObjet },
+                { NbrePotion: nombrePotionNew }
               );
-              const newMessage = `Tu viens d'utiliser ta potion. Tu as 5 tours de boost sur tous tes jets`;
+              await ficheBagPerso.updateMany(
+                { _id: IdPersoAppliqueObjet },
+                { Tour: 0 },
+                { $set: { "Tour.0": 5 } }
+              );
+              if (IdPersoAppliqueObjet == user.id) {
+                const newMessage = `Tu viens d'utiliser une potion. Tu as 5 tours de boost sur tous tes jets`;
+                await interaction.editReply({
+                  content: newMessage,
+                });
+              } else {
+                const newMessage =
+                  `<@` +
+                  IdPersoAppliqueObjet +
+                  "!" +
+                  `<@` +
+                  user.id +
+                  `vient de te donner une potion qui te booste.\n Tes 5 prochains jets sont augmentés de 5`;
+                await interaction.editReply({
+                  content: newMessage,
+                });
+              }
+            } else {
+              console.log("PAS DE POTION");
+              const newMessage = `Tu n'as pas de potion. Il te faut d'abord aller en acheter`;
               await interaction.editReply({
                 content: newMessage,
               });
+            }
+          }
+        } else if (interaction.options.getString("categorie") === "poison") {
+          let fiche = await fichePerso.findOne({
+            _id: IdPersoAppliqueObjet,
+          });
+          let ficheSac = await ficheBagPerso.findOne({
+            _id: IdPersoAppliqueObjet,
+          });
+          const nombrePotionOld = ficheSac.NbrePotion;
+          if (interaction.member.roles.cache.has(authId.RoleRP.TheLiang)) {
+            const newMessage = `Tu as déjà une potion en cours`;
+            await interaction.editReply({
+              content: newMessage,
+            });
+          } else {
+            if (nombrePotionOld > 0) {
+              interaction.member.roles.add(authId.RoleRP.TheLiang);
+              var nombrePotionNew = nombrePotionOld - 1;
+              await ficheBagPerso.updateMany(
+                { _id: IdPersoAppliqueObjet },
+                { $pull: { Sac: { $in: [`${nombrePotionOld} Potion(s)`] } } }
+              );
+              await ficheBagPerso.findOneAndUpdate(
+                { _id: IdPersoAppliqueObjet },
+                { $push: { Sac: `${nombrePotionNew} Potion(s)` } }
+              );
+              await ficheBagPerso.findOneAndUpdate(
+                { _id: IdPersoAppliqueObjet },
+                { NbrePotion: nombrePotionNew }
+              );
+              await ficheBagPerso.updateMany(
+                { _id: IdPersoAppliqueObjet },
+                { Tour: 0 },
+                { $set: { "Tour.0": 5 } }
+              );
+              if (IdPersoAppliqueObjet == user.id) {
+                const newMessage =
+                  `<@` +
+                  user.id +
+                  `! Tu viens de t'appliquer un poison par erreur qui t'affaiblit.\n Tes 5 prochains jets sont affaiblis de 5`;
+                await interaction.editReply({
+                  content: newMessage,
+                });
+              } else {
+                const newMessage =
+                  `<@` +
+                  IdPersoAppliqueObjet +
+                  "!" +
+                  `<@` +
+                  user.id +
+                  `vient de te donner un poison qui t'affaiblit.\n Tes 5 prochains jets sont affaiblis de 5`;
+                await interaction.editReply({
+                  content: newMessage,
+                });
+              }
             } else {
               console.log("PAS DE POTION");
               const newMessage = `Tu n'as pas de potion. Il te faut d'abord aller en acheter`;
