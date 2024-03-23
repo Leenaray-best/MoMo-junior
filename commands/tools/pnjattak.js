22cfb888197835f688a8002c286fccf837605434
Leena <audrey.trova@gmail.com>
2024-03-23 12:01:31 +0100

    rollPNJ

---
+++ commands/tools/pnjattak.js
@@ -0,0 +1,78 @@
+const { channel } = require("diagnostics_channel");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+var authId = JSON.parse(fs.readFileSync("./auth.json"));
+const mongoose = require("mongoose");
+const ficheBonus = require("../../BonusRollMeteo");
+const ficheSalonBonusLieu = require("../../salonBonus");
+const fichePerso = require("../../FichePerso");
+const ficheMeteo = require("../../meteo");
+const ficheMeteotest = require("../../salonMeteo");
+
+const ficheBagPerso = require("../../fichePersoSac");
+const wait = require("node:timers/promises").setTimeout;
+
+function Rand(valeur) {
+  return Math.floor(Math.random() * valeur + 1);
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pnjattak")
+    .setDescription("Attaque par un PNJ")
+    .addNumberOption((option) =>
+      option
+        .setName("valuecarac")
+        .setDescription("Valeur du niveau en Maitrise ou Competence")
+        .setRequired(true)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("bonusmj")
+        .setDescription("Valeur du bonus/malus attribué par la MJ")
+        .setRequired(false)
+    ),
+  async execute(interaction, client) {
+    const message = await interaction.deferReply({
+      fetchReply: true,
+    });
+    // if (!interaction.isChatInputCommand()) return;
+    const user = interaction.user;
+    const channelMessage = interaction.channelId;
+    console.log(user.id);
+
+    if (interaction.commandName === "pnjattak") {
+      if (
+        (user.id == authId.staff.emi ||
+          interaction.member.roles.cache.has(authId.RoleRP.RoleStaff)) &&
+        (interaction.channelId == authId.Salon.JetDeDes ||
+          interaction.channelId == authId.Salon.SalonBotAdmin)
+      ) {
+        var valRandom = Rand(20);
+        console.log(valRandom);
+        var valueCarac = interaction.options.getNumber("valuecarac");
+        console.log(valueCarac);
+        var BonusMJ = 0;
+        if (interaction.options.getNumber("bonusmj")) {
+          console.log("On a mis un Bonus au PNJ");
+          var BonusMJ = interaction.options.getNumber("bonusmj");
+          console.log(BonusMJ);
+        } else {
+          console.log("On a PAS mis un Bonus au PNJ");
+          var BonusMJ = 0;
+        }
+        var valTotal = valRandom + valueCarac + BonusMJ;
+        console.log(valTotal);
+        const newMessage = `Ton PNJ a fait un roll de ${valTotal}.`;
+        await interaction.editReply({
+          content: newMessage,
+        });
+      } else {
+        const newMessage = `Soit tu n'as pas les autorisations pour faire ça, ou tu n'es pas dans la bon salon.`;
+        await interaction.editReply({
+          content: newMessage,
+        });
+      }
+    }
+  },
+};
