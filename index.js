15b5c70e1631942bef9a390955c13534d87ef846
Leena <audrey.trova@gmail.com>
2024-03-30 03:31:47 +0100

    CronFinal

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ var cron = require("node-cron");
 //   );
 // });
 
-cron.schedule("47 * * * *", async () => {
+cron.schedule("* 8 * * *", async () => {
   try {
     const mongoClient = new MongoClient(process.env["MONGODB_URI"], {});
     await mongoClient.connect();
