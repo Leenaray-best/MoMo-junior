83c38a89ae0f500bbd17534735d33d6aa2175520
Leena <audrey.trova@gmail.com>
2024-05-12 23:37:26 +0200

    UpdateSave

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ var cron = require("node-cron");
 //   );
 // });
 
-cron.schedule("37 20 * * *", async () => {
+cron.schedule("39 21 * * *", async () => {
   try {
     const mongoClient = new MongoClient(process.env["MONGODB_URI"], {});
     await mongoClient.connect();
